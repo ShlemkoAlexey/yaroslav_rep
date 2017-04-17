fdb342a913e851ce37ce5fb47970e31d4bb2cb9b
Alex <shlemko.alexey@gmail.com>
2017-04-18 00:55:58 +0300

    13

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(".company-and-contacts").on("click", function(){
   $(".company-and-contacts").css("cursor", "normal");
   $(".company-and-contacts-drop").fadeIn();
+  $(".company-and-contacts b").addClass("rotate180");
 });
 
 $(".company-and-contacts").on("mouseleave ", function(){
   $(".company-and-contacts-drop").fadeOut();
   $(".company-and-contacts").css("cursor", "pointer");
+  $(".company-and-contacts b").removeClass("rotate180");
 });
